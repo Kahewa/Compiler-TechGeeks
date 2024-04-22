dfd4c87e9763594c252edd1706a70e3fc751c34f
Kahewa <gracepshuuya@gmail.com>
2024-04-22 12:18:04 +0200

    update

--- Compiler1.java
+++ Compiler1.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Lab08_A {
+public class Compiler1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("A MINI COMPILER PROJECT FOR CTE711S\n===================================");
