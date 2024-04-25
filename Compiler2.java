1f5f5772db0195dd4c74e3e73a7593139416b882
mortimer <167344057+morty-14@users.noreply.github.com>
2024-04-25 09:10:01 +0200

    Update Compiler2.java

--- Compiler2.java
+++ Compiler2.java
@@ -174,7 +174,7 @@ public class Compiler2 {
             tokenNumber++;
         }
         System.out.println("Total number of Tokens: " + tokens.size());
-        System.out.println("GIVEN THE GRAMMAR: E=E1 | E=E1*E2 | E=E1+E2 | E=digit | E={0,1,2,3,4,5,6,7,8,9}");
+        System.out.println("GIVEN THE GRAMMAR: E=E | E=E/E | E=E*E  | E=E+E  | E=E-E | E={|A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z|}");
     }
 
     // Inner class to represent Token
